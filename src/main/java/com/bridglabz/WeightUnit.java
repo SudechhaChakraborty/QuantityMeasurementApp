e314199b262f01810b573449c0e2d781861bfaf8
Sudechha <sudechhachakraborty@gmail.com>
2026-03-02 15:25:34 +0530

    [Sudechha] : [Added] : UC10: Implement Generic Quantity<U> with IMeasurable interface

--- src/main/java/com/bridglabz/WeightUnit.java
+++ src/main/java/com/bridglabz/WeightUnit.java
@@ -1,6 +1,6 @@
 package com.bridglabz;
 
-public enum WeightUnit {
+public enum WeightUnit implements IMeasurable {
 
     KILOGRAM(1.0),
     GRAM(0.001),
@@ -12,11 +12,23 @@ public enum WeightUnit {
         this.conversionFactor = conversionFactor;
     }
 
+    @Override
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+
+    @Override
     public double convertToBaseUnit(double value) {
         return value * conversionFactor;
     }
 
+    @Override
     public double convertFromBaseUnit(double baseValue) {
         return baseValue / conversionFactor;
+    }
+
+    @Override
+    public String getUnitName() {
+        return this.name();
     }
 }
